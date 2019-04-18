c3fa5190d7116aed18f6d7e13ecd097ee2f2861f
Prashanth <yvprashanth@gmail.com>
2019-04-18 13:56:47 -0700

    Get data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,69 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response : '',
+    post: '', 
+    responseToPost: ''
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => (
+        this.setState({ response: res }))
+      )
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/posts', {
+      method: 'GET', 
+      headers: {
+        'Authorization': 'kjhkjhkjh', 
+        'Content-Type': 'application/json'
+      }
+    });
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/categories', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'jhskjhkjeh'
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    this.setState({ responseToPost: body });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        {this.state.response && this.state.response.map(function(obj){
+          return <div>
+            {obj.title}
+          </div>
+        })}
+        </p>
+        <form onSubmit={this.handleSubmit}>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <strong>Post to Server:</strong>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
       </div>
     );
   }
